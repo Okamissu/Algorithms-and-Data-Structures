eca9a50af7dd418919369ebdf30899eaa256503b
Okamissu <okamissu@gmail.com>
2026-07-07 08:48:23 +0200

    feat(recap): implement binary search recursive function

--- Recap/binarySearch.js
+++ Recap/binarySearch.js
@@ -23,3 +23,27 @@ console.log(binarySearch([5, 10, 15, 20], 15)) // 2
 console.log(binarySearch([5, 10, 15, 20], 5)) // 0
 console.log(binarySearch([5, 10, 15, 20], 20)) // 3
 console.log(binarySearch([5, 10, 15, 20], 100)) // -1
+
+const binarySearchRecursive = (
+  arr,
+  target,
+  left = 0,
+  right = arr.length - 1,
+) => {
+  if (left > right) return -1
+
+  const mid = Math.floor(left + (right - left) / 2)
+
+  if (arr[mid] === target) return mid
+
+  if (target < arr[mid]) {
+    return binarySearchRecursive(arr, target, left, mid - 1)
+  }
+
+  return binarySearchRecursive(arr, target, mid + 1, right)
+}
+
+console.log(binarySearchRecursive([5, 10, 15, 20], 15)) // 2
+console.log(binarySearchRecursive([5, 10, 15, 20], 5)) // 0
+console.log(binarySearchRecursive([5, 10, 15, 20], 20)) // 3
+console.log(binarySearchRecursive([5, 10, 15, 20], 100)) // -1
