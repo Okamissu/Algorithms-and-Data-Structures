5c5ce08c6c571edafb40806d27f5abb75900df2d
Okamissu <okamissu@gmail.com>
2026-08-01 10:13:13 +0200

    feat: add remove method in BST

--- BinarySearchTree/binarySearchTree.js
+++ BinarySearchTree/binarySearchTree.js
@@ -1,33 +1,33 @@
 class BinarySearchTree {
   constructor() {
-    this.root = null
+    this.root = null;
   }
 
   insert(value) {
-    const newNode = new Node(value)
+    const newNode = new Node(value);
 
     if (!this.root) {
-      this.root = newNode
-      return this
+      this.root = newNode;
+      return this;
     }
 
-    let currentNode = this.root
+    let currentNode = this.root;
 
     while (true) {
       if (value < currentNode.value) {
         if (!currentNode.left) {
-          currentNode.left = newNode
-          return this
+          currentNode.left = newNode;
+          return this;
         }
 
-        currentNode = currentNode.left
+        currentNode = currentNode.left;
       } else {
         if (!currentNode.right) {
-          currentNode.right = newNode
-          return this
+          currentNode.right = newNode;
+          return this;
         }
 
-        currentNode = currentNode.right
+        currentNode = currentNode.right;
       }
     }
   }
@@ -35,73 +35,100 @@ class BinarySearchTree {
   insertRecursive(value) {
     const insertNode = (node) => {
       if (!node) {
-        return new Node(value)
+        return new Node(value);
       }
 
       if (value < node.value) {
-        node.left = insertNode(node.left)
+        node.left = insertNode(node.left);
       } else {
-        node.right = insertNode(node.right)
+        node.right = insertNode(node.right);
       }
 
-      return node
-    }
+      return node;
+    };
 
-    this.root = insertNode(this.root)
-    return this
+    this.root = insertNode(this.root);
+    return this;
   }
 
   find(value) {
-    let currentNode = this.root
+    let currentNode = this.root;
 
     while (currentNode) {
       if (currentNode.value === value) {
-        return currentNode
+        return currentNode;
       }
 
       currentNode =
-        value < currentNode.value ? currentNode.left : currentNode.right
+        value < currentNode.value ? currentNode.left : currentNode.right;
     }
 
-    return false
+    return false;
   }
 
   findRecursive(value) {
     const findNode = (node) => {
       if (!node) {
-        return false
+        return false;
       }
 
       if (node.value === value) {
-        return node
+        return node;
       }
 
-      return value < node.value ? findNode(node.left) : findNode(node.right)
+      return value < node.value ? findNode(node.left) : findNode(node.right);
+    };
+
+    return findNode(this.root);
+  }
+
+  getInorderSuccessor(currentNode) {
+    currentNode = currentNode.right;
+    while (currentNode && currentNode.left) {
+      currentNode = currentNode.left;
+    }
+    return currentNode;
+  }
+
+  remove(value, currentNode = this.root) {
+    if (!currentNode) return currentNode;
+
+    if (value < currentNode.value) {
+      currentNode.left = this.remove(value, currentNode.left);
+    } else if (value > currentNode.value) {
+      currentNode.right = this.remove(value, currentNode.right);
+    } else {
+      if (!currentNode.right) return currentNode.left;
+      if (!currentNode.left) return currentNode.right;
+
+      const successor = this.getInorderSuccessor(currentNode);
+      currentNode.value = successor.value;
+      currentNode.right = this.remove(successor.value, currentNode.right);
     }
 
-    return findNode(this.root)
+    return currentNode;
   }
 }
 
 class Node {
   constructor(value) {
-    this.value = value
-    this.left = null
-    this.right = null
+    this.value = value;
+    this.left = null;
+    this.right = null;
   }
 }
 
-const bst = new BinarySearchTree()
+const bst = new BinarySearchTree();
 
-bst.insert(10)
-bst.insert(5)
-bst.insert(15)
-bst.insert(2)
-bst.insert(7)
-bst.insert(12)
-bst.insert(20)
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(2);
+bst.insert(7);
+bst.insert(12);
+bst.insert(20);
 
-console.log(bst.find(12))
-console.log(bst.findRecursive(12))
-console.log(bst.find(100))
-console.log(bst.findRecursive(100))
+console.log(bst.find(12));
+console.log(bst.findRecursive(12));
+console.log(bst.find(100));
+console.log(bst.findRecursive(100));
