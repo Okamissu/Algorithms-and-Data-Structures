61c6d2003968e620d5338c9fc51cd707a05bce3e
Okamissu <okamissu@gmail.com>
2026-08-07 10:48:21 +0200

    feat: implement CircularQueue class with basic operations

---
+++ Stack_Queue/circularQueue.js
@@ -0,0 +1,26 @@
+class CircularQueue {
+  constructor(capacity) {
+    this.items = new Array(capacity);
+    this.capacity = capacity;
+    this.currentLength = 0;
+    this.front = -1;
+    this.rear = -1;
+  }
+
+  isFull() {
+    return this.currentLength === this.capacity;
+  }
+
+  isEmpty() {
+    return this.currentLength === 0;
+  }
+
+  enqueue(element) {
+    if (!this.isFull()) {
+      this.rear = this.rear + 1;
+      this.items[this.rear] = element;
+      this.currentLength += 1;
+      if (this.front === -1) this.front = this.rear;
+    }
+  }
+}
