7c6090520109d5fcc0816c6e012dacf4ad3604a3
Okamissu <kamil.m.kobylarz@gmail.com>
2026-08-28 08:11:50 +0200

    feat: implement simpleHash function for basic hashing

---
+++ HashTable/hashTable.js
@@ -0,0 +1,11 @@
+function simpleHash(key, arrayLen) {
+  let total = 0;
+  for (let char of key.toLowerCase()) {
+    let value = char.charCodeAt(0) - 96;
+    console.log(value);
+    total = (total + value) & arrayLen;
+  }
+  return total;
+}
+
+console.log(simpleHash('Leia', 7));
