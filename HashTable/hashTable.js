c3aaa639dcdadc2b3222e6dc445d73d162a8bde1
Okamissu <kamil.m.kobylarz@gmail.com>
2026-08-31 08:44:53 +0200

    feat: implement refinedHash function

--- HashTable/hashTable.js
+++ HashTable/hashTable.js
@@ -8,3 +8,17 @@ function simpleHash(key, arrayLen) {
 }
 
 console.log(simpleHash('Leia', 7));
+
+function refinedHash(key, arrayLen) {
+  let total = 0;
+  const WEIRD_PRIME = 31;
+
+  for (let i = 0; i < Math.min(key.length, 100); i++) {
+    let char = key[i].toLowerCase();
+    let value = char.charCodeAt(0) - 96;
+
+    total = (total * WEIRD_PRIME + value) % arrayLen;
+  }
+
+  return total;
+}
