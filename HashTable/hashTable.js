349daf4ba3f479ad0da030907e2b14ae9074cfd0
Okamissu <kamil.m.kobylarz@gmail.com>
2026-08-31 08:14:09 +0200

    fix: correct modulus operator in simpleHash function

--- HashTable/hashTable.js
+++ HashTable/hashTable.js
@@ -2,8 +2,7 @@ function simpleHash(key, arrayLen) {
   let total = 0;
   for (let char of key.toLowerCase()) {
     let value = char.charCodeAt(0) - 96;
-    console.log(value);
-    total = (total + value) & arrayLen;
+    total = (total + value) % arrayLen;
   }
   return total;
 }
