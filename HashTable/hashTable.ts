ffd00f93c71f49c90f10473e0920ec2b25b9658d
Okamissu <kamil.m.kobylarz@gmail.com>
2026-09-02 08:48:52 +0200

    refactor: remove unused hash functions and related comments

--- HashTable/hashTable.ts
+++ HashTable/hashTable.ts
@@ -21,28 +21,3 @@ class HashTable<K extends string = string, V = any> {
     return total;
   }
 }
-
-// function simpleHash(key, arrayLen) {
-//   let total = 0;
-//   for (let char of key.toLowerCase()) {
-//     let value = char.charCodeAt(0) - 96;
-//     total = (total + value) % arrayLen;
-//   }
-//   return total;
-// }
-
-// console.log(simpleHash('Leia', 7));
-
-// function refinedHash(key, arrayLen) {
-//   let total = 0;
-//   const WEIRD_PRIME = 31;
-
-//   for (let i = 0; i < Math.min(key.length, 100); i++) {
-//     let char = key[i].toLowerCase();
-//     let value = char.charCodeAt(0) - 96;
-
-//     total = (total * WEIRD_PRIME + value) % arrayLen;
-//   }
-
-//   return total;
-// }
