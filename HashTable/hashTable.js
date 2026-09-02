3363951c7dc4534696f5205d4347177665823f6e
Okamissu <kamil.m.kobylarz@gmail.com>
2026-09-02 08:27:59 +0200

    feat: implement HashTable class with private hash method

--- HashTable/hashTable.js
+++ HashTable/hashTable.js
@@ -1,24 +1,43 @@
-function simpleHash(key, arrayLen) {
-  let total = 0;
-  for (let char of key.toLowerCase()) {
-    let value = char.charCodeAt(0) - 96;
-    total = (total + value) % arrayLen;
-  }
-  return total;
-}
-
-console.log(simpleHash('Leia', 7));
-
-function refinedHash(key, arrayLen) {
-  let total = 0;
-  const WEIRD_PRIME = 31;
-
-  for (let i = 0; i < Math.min(key.length, 100); i++) {
-    let char = key[i].toLowerCase();
-    let value = char.charCodeAt(0) - 96;
-
-    total = (total * WEIRD_PRIME + value) % arrayLen;
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
   }
 
-  return total;
+  #hash(key) {
+    let total = 0;
+    const WEIRD_PRIME = 31;
+
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(0) - 96;
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+
+    return total;
+  }
 }
+
+// function simpleHash(key, arrayLen) {
+//   let total = 0;
+//   for (let char of key.toLowerCase()) {
+//     let value = char.charCodeAt(0) - 96;
+//     total = (total + value) % arrayLen;
+//   }
+//   return total;
+// }
+
+// console.log(simpleHash('Leia', 7));
+
+// function refinedHash(key, arrayLen) {
+//   let total = 0;
+//   const WEIRD_PRIME = 31;
+
+//   for (let i = 0; i < Math.min(key.length, 100); i++) {
+//     let char = key[i].toLowerCase();
+//     let value = char.charCodeAt(0) - 96;
+
+//     total = (total * WEIRD_PRIME + value) % arrayLen;
+//   }
+
+//   return total;
+// }
